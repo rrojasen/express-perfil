17e954cb000504111f84d093875a7e35967bea9b
Ray Rojas <enciso.leo@gmail.com>
2026-06-01 20:34:35 -0500

    retornando html

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -6,7 +6,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/hello", (req, res) => res.send("<h1 style='color: red;'>Hola Mundo</h1>"));
 
 api.use("/api/", router);
 
